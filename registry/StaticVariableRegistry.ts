a0da3061017ac737edaf3a94d904e8df6c31b53b
Morgan Thomas <m@m0rg.dev>
2021-09-16 17:44:22 -0700

    clean slate

--- registry/StaticVariableRegistry.ts
+++
@@ -1,7 +0,0 @@
-import { FunctionConstruct } from "../unified_parser/FunctionConstruct";
-import { TypeObject } from "../unified_parser/TypeObject";
-
-export const StaticVariableRegistry = new Map<string, { type: TypeObject, initializer?: StaticInitializer }>();
-export const StaticFunctionRegistry = new Map<string, FunctionConstruct>();
-
-export class StaticInitializer { };
